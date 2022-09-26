5984f39abf5e337faae2268b55c7040ed548fa79
jo <95hyouka@gmail.com>
2022-09-27 02:03:26 +0900

    make github ci to build and push images

---
+++ build-and-push.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+readonly TAGS=('14.5.0' '14.5.0-debian-11-r15')
+
+DOCKER_PASSWORD="$1";
+
+for TAG in "${TAGS[@]}"; do
+    docker build --build-arg IMAGE_BASE="bitnami/postgresql:$TAG" -f Dockerfile . -t "lyra95/postgres-bigm:$TAG"
+done
+
+for TAG in "${TAGS[@]}"; do
+    docker push "lyra95/postgres-bigm:$TAG"
+done
